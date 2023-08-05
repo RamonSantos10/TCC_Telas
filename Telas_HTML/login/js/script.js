48589659f52f03eb043165398dd47a2243227cca
Ramon dos Santos <ramontedescodossantos2020@gmail.com>
2023-08-05 17:28:31 -0300

    Animações de cor na tela de login e mostrar/ocultar senha com o olhinho

--- Telas_HTML/login/js/script.js
+++ Telas_HTML/login/js/script.js
@@ -1,16 +1,72 @@
+const emailInput = document.querySelector(".email");
 const passwordInput = document.querySelector(".password");
+
 const eyeIcon = document.querySelector(".olho");
+const lockIcon = document.querySelector(".cadeado");
+const card = document.querySelector(".carta");
+
 
 eyeIcon.addEventListener("click", () => {
   if (passwordInput.type === "password") {
+
     passwordInput.type = "text";
 
     eyeIcon.classList.remove("fa-eye");
     eyeIcon.classList.add("fa-eye-slash");
-  }else if (passwordInput.type === "text") {
-    passwordInput.type = "password";
+    eyeIcon.style.opacity = '50%';
 
+}else if (passwordInput.type === "text") {
+
+    passwordInput.type = "password";
+    
     eyeIcon.classList.remove("fa-eye-slash");
     eyeIcon.classList.add("fa-eye");
+    eyeIcon.style.opacity = '100%';
   }
 });
+
+
+emailInput.addEventListener('focus', () => {
+    emailInput.style.border = '2px solid #8257E5'
+    card.style.color = '#8257E5';
+})
+
+emailInput.addEventListener('blur', () => {
+    emailInput.style.border = '2px solid black'
+    card.style.color = '#202024'
+})
+
+
+
+passwordInput.addEventListener('focus', () => {
+    passwordInput.style.border = '2px solid #8257E5';
+    lockIcon.style.color = '#8257E5';
+    
+})
+
+passwordInput.addEventListener('blur', () => {
+    passwordInput.style.border = '2px solid black'
+    lockIcon.style.color = '#202024'
+})
+
+
+
+
+
+/*
+if (document.hasFocus == passwordInput) {
+    passwordInput.style.border = '0px solid #8257E5'
+}else {
+    passwordInput.style.border = '2px solid #8257E5'
+}
+*/
+
+
+/*
+passwordInput.addEventListener('focus', () => {
+    passwordInput.style.border = '2px solid #8257E5'
+})
+passwordInput.addEventListener('leave', () => {
+    passwordInput.style.border = '0px solid #8257E5'
+})
+*/
